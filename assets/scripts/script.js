a8fff680c69235e27b97c7125a82023ff030aefe
Kurt Bixby <kurtbixby@mac.com>
2022-06-23 13:37:29 -0500

    Commented JS

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -20,10 +20,14 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// Character classes used in password generation
 var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 var numericCharacters = "0123456789";
 var letters = "abcdefghijklmnopqrstuvwxyz";
 
+// Main wrapper function used to generate a password
+// Returns null on failure
+// Returns a string upon on success
 function generatePassword() {
   var length = askForLength(8, 128);
   // Invalid length
@@ -36,7 +40,6 @@ function generatePassword() {
   var numbers = askForCharacter("numeric characters");
   var special = askForCharacter("special characters");
 
-
   // No character sets
   if (!(lowercase || uppercase || numbers || special)) {
     window.alert("Invalid password format. Please try again and select at least one character class for your password.");
@@ -47,21 +50,27 @@ function generatePassword() {
   return generatedPassword;
 }
 
+// Function that asks the user for the desired password length and checks for validity
+// Alerts the user to try again with a different response and returns null upon failure
+// Returns a number on success
 function askForLength(minLength, maxLength) {
   var lengthInput = window.prompt("How long would you like your password? (Between " + minLength + " and " + maxLength +  " characters)");
-  if (typeof lengthInput !== 'string') {
+  // Validate input can be a number
+  if (isNaN(lengthInput)) {
     window.alert(lengthInput + " is not a number. Please try again and enter a number.")
     return null;
   }
-
   var numInput = Number(lengthInput);
+
+  // Validate input is an integer
   if (!Number.isInteger(numInput)) {
     window.alert(numInput + " is not an integer. Please try again and enter an integer.")
     return null;
   }
-
   var inputInt = Number.parseInt(lengthInput);
   // Input is an integer
+
+  // Validate length
   if (inputInt < minLength) {
     window.alert(inputInt + " is smaller than " + minLength + ". Please try again and enter a number between " + minLength + " and " + maxLength + ".");
     return null;
@@ -74,10 +83,16 @@ function askForLength(minLength, maxLength) {
   return inputInt;
 }
 
+// Prompt the user to include a certain character class
+// Uses character type in the message to be displayed.
+// Return a boolean
 function askForCharacter(characterType) {
   return window.confirm("Include " + characterType + "? (OK for yes, Cancel for no)");
 }
 
+// Main password generation engine
+// Arguments: a length number and a series of booleans representing each character class
+// Returns: a string of the generated password
 function passwordGeneration(length, lowercase, uppercase, numbers, special) {
   var myCharacters = "";
   var characterCount = 0;
@@ -113,15 +128,17 @@ function passwordGeneration(length, lowercase, uppercase, numbers, special) {
     characterCount++;
   }
 
-  return buildPassword(myCharacters);
+  return shuffleCharacters(myCharacters);
 }
 
+// Returns a random character from a given string
 function getCharacter(characterSet) {
   var charIndex = Math.floor(Math.random() * characterSet.length);
   return characterSet.charAt(charIndex);
 }
 
-function buildPassword(characterSet) {
+// Returns a new string made from shuffled characters of an original string
+function shuffleCharacters(characterSet) {
   var finalPassword = "";
 
   var characterArray = Array.from(characterSet);
